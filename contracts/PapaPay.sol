202223185ec14b4c6ddc6cfc1b02c5f1e77cc7fe
carnisman <msolovey@gmail.com>
2021-11-03 17:34:37 -0300

    Actu

--- contracts/PapaPay.sol
+++ contracts/PapaPay.sol
@@ -19,65 +19,81 @@ contract PapaPay {
     // Course number (unique)
     uint papaCourse;
     // Total price for the course
-    uint price;
+    uint papaPrice;
     // Amount of lessons
     uint papaLessons;
     // Timelock period in days. After that the student can recover his tokens
     uint papaLock;
     // Can we use the enum to check the given lessons?
     State state;
-    // Teache and student addresses
-    address payable tutor;
-    address payable student;
+    // Balance of Course
+    uint papaBalance;
+    // Teacher and student addresses
+    address payable papaTutor;
+    address payable papaStudent;
   }
+
+  event CourseCreated (uint papaCourse);
 
   constructor() public {
   }
-
-  function papaCreate(address _tutor) {
-
 	// Tutor creates the course, wich includes number of lessons, total cost, address of student and a time lock
+  // Requires price != 0
+  function papaCreate( uint _papaDesc, uint _papaPrice, uint _papaLessons, uint _papaLock, address _papaStudent ) public returns (bool) {
+    papas[papaCourse] = Papa({
+      papaDesc: _papaDesc,
+      papaCourse: papaCount,
+      papaPrice: _price,
+      papaLessons: _papaLessons,
+      papaLock: _papaLock,
+      papaTutor: msg.sender,
+      papaStudent: _papaStudent
+       });
+    emit CourseCreated(papaCount);
+    papaCount=papaCount +1;
+    return true;
+  }
 
-  };
+  // Student approves that contract, the total cost of the course is deducted from student wallet and stored on the contract
+  // Pensar requires: papaStudent=msg.sender, papaBalance=0
+  function papaApprove(address _student, uint _papaCourse) public payable {
 
-  function papaApprove(address _student, uint _papaCourse) {
+	
 
-	// Student approves that contract, the total cost of the course is deducted from student wallet and stored on the contract
+  }
 
-  };
-
-  function papaView(address _anyaddress) {
+  function papaView(address _anyaddress) public view {
 
 	// List all courses for any given address
 
-  };
+  }
 
   function papaInit(address _tutor, uint _papaCourse) {
 
   // The tutor calls for the start of an individual lesson
 
-  };
+  }
 
   function papaAssist(address _student, uint _papaCourse) {
 
   // The student is giving attendance, making possible a partial withdrawn for the teacher
 
-  };
+  }
 
   function papaWithdrawn(address _tutor, uint _papaCourse) {
 
   // The teacher can make withdrawns from here
 
-  };
+  }
 
   function papaRecover(address _student, uint _papaCourse) {
 
   // If a time lock for unfulfillment is set, after that period of time, the student can recover all of his remaining tokens
 
-  };
+  }
 
   function () {
   //  A fallback here
-  };
+  }
 
 }
