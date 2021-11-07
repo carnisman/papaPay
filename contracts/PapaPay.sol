bf0c3651bd926f9584b6368b246bd6c41fb7d766
Marcelo Solovey <msolovey@gmail.com>
2021-11-07 18:36:15 -0300

    Cambios 3

--- contracts/PapaPay.sol
+++ contracts/PapaPay.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract PapaPay {
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract PapaPay is ReentrancyGuard {
+  
+  using ECDSA for bytes32;
 
   uint public papaCount;
 
   mapping (uint => Papa) public papas;
 
-  // <enum State: ForSale, Sold, Shipped, Received>
-
-  enum State {LessonStarted, LessonAssist, CourseDeposit, CourseWithdrawn}
+  bool locked = false;
 
   // <struct Item: name, sku, price, state, seller, and buyer>
 
@@ -24,8 +27,6 @@ contract PapaPay {
     uint papaLessons;
     // Timelock period in days. After that the student can recover his tokens
     uint papaLock;
-    // Can we use the enum to check the given lessons?
-    State state;
     // Balance of Course
     uint papaBalance;
     // Teacher and student addresses
@@ -33,13 +34,37 @@ contract PapaPay {
     address payable papaStudent;
   }
 
+//
+//    EVENTS
+//
+
   event CourseCreated (uint papaCourse);
   event CourseDeposit (uint papaCourse);
 
+//
+//    MODIFIERS
+//
+
+
+  modifier isStudent(uint _papaCourse) {
+      require(msg.sender == papas[_papaCourse].papaStudent, "Not a student");
+      _;
+  }
+
+  modifier zeroBalance(uint _papaCourse) {
+       require(papas[_papaCourse].papaBalance == 0, "Course already funded");
+      _;
+  }
+
+  modifier exactBalance(uint _papaCourse) {
+       require(msg.value == papas[_papaCourse].papaPrice, "Not enough ETH");
+      _;
+  }
+
   constructor() public {
   }
+
 	// Tutor creates the course, wich includes number of lessons, total cost, address of student and a time lock
-  // Requires price != 0
   function papaCreate( uint _papaDesc, uint _papaPrice, uint _papaLessons, uint _papaLock, address _papaStudent ) public returns (bool) {
     papas[papaCourse] = Papa({
       papaDesc: _papaDesc,
@@ -51,17 +76,22 @@ contract PapaPay {
       papaStudent: _papaStudent
        });
     emit CourseCreated(papaCount);
-    papaCount=papaCount +1;
+    papaCount = papaCount +1;
     return true;
   }
 
   // Student approves that contract, the total cost of the course is deducted from student wallet and stored on the contract
-  // Pensar requires: papaStudent=msg.sender, papaBalance=0, msg.value=papaPrice
-  function papaApprove(address _student, uint _papaCourse) public payable {
-    require(msg.value = papas[_papaCourse].papaPrice;
-    papas[_papaCourse].papaBalance += msg.value;
-    emit CourseDeposit;
-  }
+  // Pensar requires: msg.value=papaPrice
+  function papaApprove(address _student, uint _papaCourse) 
+    public 
+    payable 
+    isStudent(_papaCourse) 
+    zeroBalance(_papaCourse) 
+    exactBalance(_papaCourse)
+    {
+      papas[_papaCourse].papaBalance += msg.value;
+      emit CourseDeposit;
+    }
 
   function papaView(address _anyaddress) public view {
 
@@ -82,7 +112,10 @@ contract PapaPay {
   }
 
   function papaWithdrawn(address _tutor, uint _papaCourse) {
-
+    require(!locked, "Reentrant call detected!");
+    locked = true;
+    ...
+    locked = false;
   // The teacher can make withdrawns from here
 
   }
