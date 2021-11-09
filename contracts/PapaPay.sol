583e9aa7d31ebba78677d159772c16d58907f82f
Marcelo Solovey <msolovey@gmail.com>
2021-11-09 02:35:49 -0300

    Cambios 6

--- contracts/PapaPay.sol
+++ contracts/PapaPay.sol
@@ -14,28 +14,26 @@ contract PapaPay is ReentrancyGuard {
 
   bool locked = false;
 
-  // <struct Item: name, sku, price, state, seller, and buyer>
-
   struct Papa {
-    // Short description of the contract 
-    string papaDesc;
-    // Course number (unique)
-    uint papaCourse;
-    // Total price for the course
-    uint papaPrice;
-    // Amount of lessons
-    uint papaLessons;
-    // Timelock period in days. After that the student can recover his tokens
-    uint papaLock;
-    // Balance of Course
-    uint papaBalance;
-    // Teacher and student addresses and signed assistance
-    address papaTutor;
-    uint papaTutorSign;
-    address papaStudent;
-    uint papaStudentSign;
-    uint papaWithdrew;
-  }
+          // Short description of the contract 
+          string papaDesc;
+          // Course number (unique)
+          uint papaCourse;
+          // Total price for the course
+          uint papaPrice;
+          // Amount of lessons
+          uint papaLessons;
+          // Timelock period in days. After that the student can recover his tokens
+          uint papaLock;
+          // Balance of Course
+          uint papaBalance;
+          // Teacher and student addresses and signed assistance
+          address papaTutor;
+          uint papaTutorSign;
+          address papaStudent;
+          uint papaStudentSign;
+          uint papaWithdrew;
+        }
 
 //
 //    EVENTS
@@ -69,7 +67,7 @@ contract PapaPay is ReentrancyGuard {
 
 	// Tutor creates the course, wich includes number of lessons, total cost, address of student and a time lock
   function papaCreate( string memory _papaDesc, uint _papaPrice, uint _papaLessons, uint _papaLock, address _papaStudent ) 
-    public 
+    external 
     returns (bool) 
     {
       papas[papaCount] = Papa({
@@ -103,48 +101,53 @@ contract PapaPay is ReentrancyGuard {
       emit CourseDeposit(_papaCourse,msg.value);
     }
 
-  function papaView(address _anyaddress) public view {
+  function papaView(address _anyaddress) 
+    external 
+    view 
+    {
 
-	// List all courses for any given address
+    // List all courses for any given address
 
-  }
+    }
 
   // The tutor calls for the start of an individual lesson
   // requires msg.sender=tutor balance!=0 // requires timestamp antes de prox firma
-  function papaInit(uint _papaCourse) 
-  external 
-  {
-    require (msg.sender == papas[_papaCourse].papaTutor);
-    require (papas[_papaCourse].papaTutorSign <= papas[_papaCourse].papaStudentSign);
-    papas[_papaCourse].papaTutorSign += 1;
-  }
+  function papaInitLesson(uint _papaCourse) 
+    external 
+    {
+      require (msg.sender == papas[_papaCourse].papaTutor, "Not a Tutor");
+      require (papas[_papaCourse].papaTutorSign != papas[_papaCourse].papaLessons, "All lessons were given");
+      require (papas[_papaCourse].papaTutorSign < papas[_papaCourse].papaStudentSign, "Student didn´t sign attendance");
+      papas[_papaCourse].papaTutorSign += 1;
+    }
   
   // The student is giving attendance, making possible a partial withdrawn for the teacher
-  function papaAssist(uint _papaCourse) 
-  external 
-  {
-    require (msg.sender == papas[_papaCourse].papaStudent);
-    require (papas[_papaCourse].papaStudentSign <= papas[_papaCourse].papaTutorSign);
-    papas[_papaCourse].papaStudentSign += 1;
-  }
+  function papaAttendLesson(uint _papaCourse) 
+    external 
+    {
+      require (msg.sender == papas[_papaCourse].papaStudent, "Not a Student");
+      require (papas[_papaCourse].papaStudentSign != papas[_papaCourse].papaLessons, "All lessons were taken");
+      require (papas[_papaCourse].papaStudentSign == papas[_papaCourse].papaTutorSign, "You already signed attendace");
+      papas[_papaCourse].papaStudentSign += 1;
+    }
 
-  function papaWithdrawn(uint _papaCourse) 
-  external
-  payable
-  {
-    require(!locked, "Reentrant call detected!");
-    require (msg.sender == papas[_papaCourse].papaTutor);
-    require (papas[_papaCourse].papaBalance != 0 && papas[_papaCourse].papaStudentSign != 0 && papas[_papaCourse].papaStudentSign != 0);
-    require (papas[_papaCourse].papaStudentSign == papas[_papaCourse].papaStudentSign);
-    require (papas[_papaCourse].papaStudentSign != papas[_papaCourse].papaWithdrew);
-    locked = true;
-    papas[_papaCourse].papaWithdrew += 1;
-    uint papaAmount = (papas[_papaCourse].papaBalance) / (papas[_papaCourse].papaLessons - papas[_papaCourse].papaWithdrew + 1);
-    papas[_papaCourse].papaBalance = papas[_papaCourse].papaBalance - papaAmount;
-    (bool sent, ) = msg.sender.call{value: papaAmount}("");
-    require(sent, "Failed to send Ether");
-    locked = false;
-  }
+  function papaWithdraw(uint _papaCourse) 
+    external
+    payable
+    {
+      require(!locked, "Reentrant call detected!");
+      require (msg.sender == papas[_papaCourse].papaTutor, "Not a Tutor");
+      require (papas[_papaCourse].papaBalance != 0 && papas[_papaCourse].papaTutorSign != 0,"Check course balance, and Tutor or Student lesson signatures");
+      //require (papas[_papaCourse].papaStudentSign == papas[_papaCourse].papaTutorSign,"You didn´t start your last lesson");
+      require (papas[_papaCourse].papaTutorSign != papas[_papaCourse].papaWithdrew,"Already withdrawn your last lesson");
+      locked = true;
+      papas[_papaCourse].papaWithdrew += 1;
+      uint papaAmount = (papas[_papaCourse].papaPrice / papas[_papaCourse].papaLessons) * (papas[_papaCourse].papaWithdrew -  papas[_papaCourse].papaTutorSign);
+      papas[_papaCourse].papaBalance = papas[_papaCourse].papaBalance - papaAmount;
+      (bool sent, ) = msg.sender.call{value: papaAmount}("");
+      require(sent, "Failed to send Ether");
+      locked = false;
+    }
 
   function papaRecover(address _student, uint _papaCourse) public {
 
