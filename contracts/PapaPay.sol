e4d1c5414512bd0c39802d4085ff5b70baaf2e19
Marcelo Solovey <msolovey@gmail.com>
2021-11-23 00:28:32 -0300

    Actualizando test

--- contracts/PapaPay.sol
+++ contracts/PapaPay.sol
@@ -159,5 +159,16 @@ contract PapaPay is ReentrancyGuard {
   fallback () external {
 ///  A fallback here
   }
-
+  /// TEST TEST TEST
+  // function fetchItem(uint _sku) public view
+  //    returns (string memory name, uint sku, uint price, uint state, address seller, address buyer)
+  //  {
+  //    name = items[_sku].name;
+  //    sku = items[_sku].sku;
+  //    price = items[_sku].price;
+  //    state = uint(items[_sku].state);
+  //    seller = items[_sku].seller;
+  //    buyer = items[_sku].buyer;
+  //    return (name, sku, price, state, seller, buyer);
+  //  }
 }
