ee83660362f55bd69ea82eb8def15edb9683b40e
carnisman <msolovey@gmail.com>
2021-11-25 21:03:11 -0300

    test

--- client/src/utils/config.js
+++
@@ -1,6 +0,0 @@
-exports.config = {
-    BASE_URL: process.env.REACT_APP_BASE_URL || '',
-    BASENAME: process.env.NODE_ENV == "development" ? "" : "/frontend/recetas",
-    MICRONAME: "recetas",
-    ENV: process.env.NODE_ENV,
-}
